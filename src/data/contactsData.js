51f67b0ddecc899c998d6aba6ac1166af11ef744
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-01-22 15:04:37 +0300

    first commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: 'miratuujaallataa@gmail.com',
+    phone: '+251 73284058',
+    address: 'Addis Ababa, Ethiopia',
+    github: 'https://github.com/mijaleta',
+    facebook: 'https://www.facebook.com/miretu.jaleta',
+    linkedIn: 'https://www.linkedin.com/in/miretu-jaleta-995b22231/',
+    twitter: 'https://twitter.com/mijaletac',
+    medium: 'https://medium.com/@miratuujaallataa',
+    stackOverflow: 'https://stackoverflow.com/users/18483703/miretu',
+    devUsername: "mijaleta"
+}
---
+++ src/data/projectsData.js
@@ -0,0 +1,47 @@
+import eight from '../assets/svg/projects/brr22.png'
+import seven from '../assets/svg/projects/brr33.png'
+import six from '../assets/svg/projects/brr11.png'
+import smsgateway from '../assets/svg/projects/sms-gateway.png'
+// import duna from '../assets/svg/projects/duna svg.svg'
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'online bus ticket ',
+        projectDesc: 'Booking the ticket for bus .',
+        tags: [ 'SAAS', 'React Bootstrap'],
+        code: '',
+        demo: 'https://miretu-jaleta.netlify.app/',
+        image: six
+    }
+    ,    {
+        id: 2,
+        projectName: 'sms gateway',
+        projectDesc: 'It is all about sms gateway .',
+        tags: [ 'SAAS', 'Bootstrap','html','css'],
+        code: '',
+        demo: 'https://message-link.bomerc.com/',
+        image: smsgateway
+    },
+    
+    {
+        id: 4,
+        projectName: 'Photo Studio',
+        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
+        tags: ['Bootstrap', 'Express', 'JavaScript'],
+        code: '#',
+        demo: 'https://duna-photo-studio.netlify.app/',
+        image: seven
+    },
+    {
+        id: 3,
+        projectName: 'Portfolio Site',
+        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
+        tags:['Bootstrap', 'Express', 'JavaScript'],
+        code: '#',
+        demo: '#',
+        image: eight
+    }
+
+]
+
+
