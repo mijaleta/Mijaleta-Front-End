8611ba289536738ecd8cf241a09889297edc9cf3
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-01-24 15:35:33 +0300

    Update portfolio metadata and images to reflect accurate personal information

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,10 +5,10 @@ export const headerData = {
     desciption: "Hello! I am Miretu Jaleta, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     // image: 'https://i.ibb.co/7bnmm3h/m12.png',
     
-    image: 'https://i.ibb.co/Xb1p0SR/mpp.jpg',
-    image2: 'https://ibb.co/7jQxHLT',
+    image: 'https://i.ibb.co/bMrWvcTD/photo-2025-07-19-07-29-43.jpg',
+    image2: 'https://i.ibb.co/bMrWvcTD/photo-2025-07-19-07-29-43.jpg',
 
-    imagebw: 'https://i.ibb.co/7bnmm3h/m12.png',
+    imagebw: 'https://ibb.co/p7tFs0g',
     resumePdf: 'https://drive.google.com/file/d/1VpIqcPse4m_EKC0HNli5oUdh_UrtWOUK/view?usp=sharing'
 }
 
