07113eaaba42bc17798f99e3e9703c60b684f96b
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-01-24 14:22:14 +0300

    Update aboutData and experienceData with accurate personal and professional details

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is Miretu Jaleta. I'm a web designer and developer based in Jimma,Oromia, Ethiopia.",
-    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React,  Redux, Bootstrap, MongoDB, Mysql, etc.",
+    description1: "My name is Miretu Jaleta. I'm a web designer and developer based in Addis Ababa, Ethiopia.",
+    description2: "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Python,tailwindcss, Django Rest Framework, Nodejs, Nestjs, React js,  Redux toolkit,redis, Bootstrap, MongoDB, Mysql, etc.",
 }
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,24 +1,24 @@
 export const experienceData = [
    
     {
-        id: 2,
-        company: 'Fiverr(freelance)',
-        jobtitle: 'Full-Stack Developer',
-        startYear: 'Jun 2021',
-        endYear: 'Jan 2022'
-    },
-    {
-        id: 3,
-        company: 'Learning New Technology For Everyday',
-        jobtitle: 'Self Learning',
-        startYear: '2018',
+        id: 0,
+        company: 'Ethiopian Artificial Intelligence Institute',
+        jobtitle: 'Full Stack Developer and Ml Engineer',
+        startYear: 'Nov 2024',
         endYear: 'Present'
     },
     {
         id: 1,
+        company: 'DrongoErp',
+        jobtitle: 'Backend Developer',
+        startYear: 'Aug 2024',
+        endYear: 'Dec 2024'
+    },
+    {
+        id: 2,
         company: 'Debo Engineering Corporate',
-        jobtitle: 'Software Engineering Intern',
-        startYear: 'June 2023',
-        endYear: 'Present'
+        jobtitle: 'AI and Web Developer',
+        startYear: 'May 2023',
+        endYear: 'Aug 2024'
     },
 ]
