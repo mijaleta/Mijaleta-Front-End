420095a7c7c6d636f3333c0e24c42a8ff75d012c
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-24 14:56:50 +0300

    updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,8 +6,26 @@ import smsgateway from '../assets/svg/projects/sms-gateway.png'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'online bus ticket ',
-        projectDesc: 'Booking the ticket for bus .',
+        projectName: 'Master Service for Microservices',
+        projectDesc: 'A master service that coordinates and provides unified access to multiple microservices in a system, simplifying communication and management.',
+        tags: [ 'SAAS', 'React Bootstrap'],
+        code: '',
+        demo: 'https://miretu-jaleta.netlify.app/',
+        image: six
+    },
+    {
+        id: 2,
+        projectName: 'Civil Case Management System',
+        projectDesc: 'A system to manage civil cases efficiently, including case registration, tracking, scheduling, and documentation for legal processes.',
+        tags: [ 'SAAS', 'React Bootstrap'],
+        code: '',
+        demo: 'https://miretu-jaleta.netlify.app/',
+        image: six
+    },
+    {
+        id: 3,
+        projectName: 'Online Bus Ticket Booking System',
+        projectDesc: 'A web/app platform that allows users to search, book, and manage bus tickets online with real-time availability and secure payments.',
         tags: [ 'SAAS', 'React Bootstrap'],
         code: '',
         demo: 'https://miretu-jaleta.netlify.app/',
@@ -15,8 +33,8 @@ export const projectsData = [
     }
     ,    {
         id: 2,
-        projectName: 'sms gateway',
-        projectDesc: 'It is all about sms gateway .',
+        projectName: 'Sms Gateway',
+        projectDesc: 'An SMS Gateway project that allows sending, receiving, and managing SMS messages programmatically for notifications or communication.',
         tags: [ 'SAAS', 'Bootstrap','html','css'],
         code: '',
         demo: 'https://message-link.bomerc.com/',
@@ -26,21 +44,13 @@ export const projectsData = [
     {
         id: 4,
         projectName: 'Photo Studio',
-        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
+        projectDesc: "A platform to manage photo studio operations, including booking sessions, storing photos, and organizing client galleries efficiently.",
         tags: ['Bootstrap', 'Express', 'JavaScript'],
         code: '#',
         demo: 'https://duna-photo-studio.netlify.app/',
         image: seven
     },
-    {
-        id: 3,
-        projectName: 'Portfolio Site',
-        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
-        tags:['Bootstrap', 'Express', 'JavaScript'],
-        code: '#',
-        demo: '#',
-        image: eight
-    }
+ 
 
 ]
 
