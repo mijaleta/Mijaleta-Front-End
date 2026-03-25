9b576065a6b23fb0584d21d6688d0803d7ed98e8
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-03-25 23:44:19 +0300

    update headerData.js to remove unused image and correct resume PDF link

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,12 +3,10 @@ export const headerData = {
     name: 'Miretu Jaleta',
     title: "Hello! I am",
     desciption: "Hello! I am Miretu Jaleta, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    // image: 'https://i.ibb.co/7bnmm3h/m12.png',
-    
     image: 'https://i.ibb.co/bMrWvcTD/photo-2025-07-19-07-29-43.jpg',
     image2: 'https://i.ibb.co/bMrWvcTD/photo-2025-07-19-07-29-43.jpg',
 
     imagebw: 'https://ibb.co/p7tFs0g',
-    resumePdf: 'https://drive.google.com/file/d/1VpIqcPse4m_EKC0HNli5oUdh_UrtWOUK/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1hEh9FMKIUU3BHLjUQ-uSIN_F0iCkAt5G/view?usp=sharing'
 }
 
